d098bbea4f4dbffdcf58956328fe3dcaab1a587a
Jitae Kim <kimjt1226@gmail.com>
2016-10-24 12:06:56 -0400

    json.js added

---
+++ Webpage/forumJsonConverter.js
@@ -0,0 +1,43 @@
+    
+    function ConvertFormToJSON(form){
+      var array = jQuery(form).serializeArray();
+      var json = {};
+      
+      jQuery.each(array, function() {
+        json[this.name] = this.value || '';
+      });
+      
+      return json;
+    }
+    
+    jQuery(document).on('ready', function() {
+      jQuery('form#add-new').bind('submit', function(event){
+        event.preventDefault();
+        
+        var form = this;
+        var json = ConvertFormToJSON(form);
+        var tbody = jQuery('#to-do-list > tbody');
+
+        $.ajax({
+          type: "POST",
+          url: "submit.php",
+          data: json,
+          dataType: "json"
+        }).always(function() { 
+          tbody.append(
+            '<tr>
+              <td>' + form['new-number'].value + '</td>
+              <td>' + form['new-title'].value + '</td>
+              <td>' + form['new-desc'].value + '</td>
+              <td>' + form['new-name'].value + '</td>
+              <td>' + form['new-data'].value + '</td>
+              <td>' + form['new-view'].value + '</td>
+              <td>' + form['new-likes'].value + '</td>
+            </tr>');
+        }).fail(function() { 
+          alert("Failed to add to-do"); 
+        });
+
+        return true;
+      });
+    });
