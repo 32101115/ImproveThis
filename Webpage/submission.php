9f680c15e82bf559f4f5809572937f87c1700b29
Jitae Kim <kimjt1226@gmail.com>
2016-10-25 16:49:29 -0400

    submission.php edited

--- Webpage/submission.php
+++ Webpage/submission.php
@@ -11,8 +11,9 @@ $like2 = $_POST['like1'];
 $connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
 $db = mysql_select_db("mydba", $connection); // Selecting Database
 if (isset($_POST['number1'])) {
-$query = mysql_query("insert into form_element(new-number, new-title, new-des, new-name, new-date, new-view, new-like) 
-values ('$number2', '$title2', '$des2','$name2','date2','view2','like2')"); //Insert Query
+$query = mysql_query("insert into form_element(new-number, new-title, new-des, 
+new-name, new-date, new-view, new-like) values ('$number2', '$title2', '$des2',
+'$name2','$date2','$view2','$like2')"); //Insert Query
 echo "Form Submitted succesfully";
 }
 mysql_close($connection); // Connection Closed
