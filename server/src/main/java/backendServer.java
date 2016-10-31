e511e549b5c0de30892817c0780b69423d4464ad
Jitae Kim <kimjt1226@gmail.com>
2016-10-31 11:10:19 -0400

    updated

--- server/src/main/java/backendServer.java
+++ server/src/main/java/backendServer.java
@@ -3,7 +3,12 @@ import static spark.Spark.get;
 public class backendServer {
     public static void main( String[] args ) {
         get( "/getImprovement/*/*/*", ( req, res ) -> {
-            return ImproveThisUtils.getImprovementSuggestion( req.splat()[0], req.splat()[1], req.splat()[2] ).toString();
+            try {
+                return ImproveThisUtils.getImprovementSuggestion(req.splat()[0], req.splat()[1], req.splat()[2]).toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         } );
         get( "/upvote/*/*/*", ( req, res ) -> {
             ImproveThisUtils.upvote( req.splat()[0], req.splat()[1], req.splat()[2] );
